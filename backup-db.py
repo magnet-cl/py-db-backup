56b75d15bd091b920419f8cf06a7f1fbbc7ab8c2
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-12-06 17:13:24 -0300

    DB dump generation implemented

--- backup-db.py
+++ backup-db.py
@@ -1,5 +1,12 @@
-from amazon_s3 import AmazonS3
 from ConfigParser import ConfigParser
+from distutils.dir_util import mkpath
+from distutils.errors import DistutilsFileError
+import gzip
+from os import remove
+from sh import pg_dump
+from time import gmtime, strftime
+
+from amazon_s3 import AmazonS3
 
 
 def parse_configuration():
@@ -12,10 +19,44 @@ def parse_configuration():
 
 def generate_backup():
     """ Generates the database dump. """
-    dump_name = ""
-    filepath = ""
 
-    return dump_name, filepath
+    print "Generating database dump."
+
+    # configuration file
+    config = parse_configuration()
+
+    # dumps folder creation
+    dumps_folder = "db_dumps"
+    try:
+        mkpath(dumps_folder)
+    except DistutilsFileError:
+        print "Error: can't mkdir {}".format(dumps_folder)
+        return False
+
+    # dump name generation
+    dump_name = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    dump_name = "{}/{}.sql".format(dumps_folder, dump_name)
+
+    # db engine
+    db_engine = config.get('db', 'engine')
+
+    if db_engine == 'postgresql':
+        pg_dump(config.get('db', 'name'), '-f', dump_name)
+    else:
+        print "Error: DB engine not supported."
+        return False
+
+    # gzip dump
+    print "Compressing database dump."
+    with open(dump_name, 'rb') as dump_file:
+        dump_gzipped = gzip.open("{}.gz".format(dump_name), 'wb')
+        dump_gzipped.writelines(dump_file)
+        dump_gzipped.close()
+
+    # remove raw file
+    remove(dump_name)
+
+    return "{}.gz".format(dump_name)
 
 
 def upload_backup():
