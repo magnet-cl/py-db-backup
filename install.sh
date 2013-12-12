2480fa4b7eb73fe375d2282fcb8cad3ebf913850
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2013-12-12 18:58:59 -0300

    Install script added
    Installs and updates pip (python package manager), then installs boto
    and sh through pip.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# install pip
+sudo apt-get install -y python-pip
+
+# update pip
+sudo pip install -U pip
+
+# required packages
+sudo pip install -U boto
+sudo pip install -U sh
